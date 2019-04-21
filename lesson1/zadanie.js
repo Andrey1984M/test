2b05c204b3daffbc2c768ad9802f81bc952b81ab
Andrey <mamitko2018@yandex.ru>
2019-04-21 18:09:03 +1000

    пятый коммит

--- lesson1/zadanie.js
+++ lesson1/zadanie.js
@@ -1,15 +1,21 @@
 var money = +prompt('Ваш бюджет на месяц?', ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
-    
+
 var a = prompt('Введите обязательную статью расходов в этом месяце', ''),
     b = prompt('Во сколько обойдется?', '');
 
 var appData = {
     budzet: money,
     timeData: time,
-    expenses: {a, b},
+    expenses: {
+        a,
+        b
+    },
     optionalExpenses: {},
     income: [],
     savings: false
 };
-alert(money/30);
+
+alert(appData.budzet / 30);
+
+alert(appData.expenses.a);
