f2072dd093e2f2c00f0a8b36107597bc0817a7ac
Andrey <mamitko2018@yandex.ru>
2019-04-21 15:56:02 +1000

    Это наш первый коммит

---
+++ lesson1/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// var leftBorder = 1;
+//let second;
+// console.log(second);
+
+
+// console.log(4 / 0);
+// console.log('string' * 4);
+//  console.log(string);
+// console.log(second);
+
+// let persone = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(persone.name);
+// console.log(persone['name']);
+
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[2]);
+
+
+//let answer = confirm ('Are you here?');
+
+// let answer = prompt('Есть ли вам 18?', 'Да');
+// console.log(typeof(null));
+
+
+//console.log(5**2);
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(incr);
+// console.log(--decr);
+
+// console.log(20 % 7);
+
+// console.log('2' == 2);
+// console.log('2' === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+
