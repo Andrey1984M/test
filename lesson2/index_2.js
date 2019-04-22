1c88de61fceb51c26573082c2ca699eb5c3608bb
Andrey <mamitko2018@yandex.ru>
2019-04-22 15:54:44 +1000

    Шестой коммит

---
+++ lesson2/index_2.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// if (2 * 4 == 7) {
+//     console.log('Верно!');
+// } else {
+//     console.log('Неверно!');
+// }
+
+
+let num = 48;
+
+// if (num < 49) {
+//     console.log('Неверно!');
+// } else if (num > 100) {
+//     console.log('Много!');
+// } else {
+
+// }
+
+// (num == 50) ? console.log('Верно!'): console.log('Неверно!');
+
+switch (true) {
+    case num < 49:
+        console.log('Неверно!');
+        break;
+    case num > 100:
+        console.log('Много!');
+        break;
+    case num > 80:
+        console.log('Все еще много!');
+        break;
+    case 50:
+        console.log('Верно!');
+        break;
+    default:
+        console.log('Что то пошло не так');
+        break;
+}
---
+++ lesson2/tempCodeRunnerFile.js
@@ -0,0 +1,29 @@
+let num = 50;
+
+// if (num < 49) {
+//     console.log('Неверно!');
+// } else if (num > 100) {
+//     console.log('Много!');
+// } else {
+
+// }
+
+// (num == 50) ? console.log('Верно!'): console.log('Неверно!');
+
+switch (true) {
+    case num < 49:
+        console.log('Неверно!');
+        break;
+    case num > 100:
+        console.log('Много!');
+        break;
+    case num > 80:
+        console.log('Все еще много!');
+        break;
+    case num == 50:
+        console.log('Верно!');
+        break;
+    default:
+        console.log('Что то пошло не так');
+        break;
+}
