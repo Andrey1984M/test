e79cec5462d82a2d30b07e3ec8f416b3e55b9412
Andrey <mamitko2018@yandex.ru>
2019-04-22 16:20:41 +1000

    Седьмой коммит

--- lesson2/index_2.js
+++ lesson2/index_2.js
@@ -7,32 +7,53 @@
 // }
 
 
-let num = 48;
+// let num = 50;
 
-// if (num < 49) {
-//     console.log('Неверно!');
-// } else if (num > 100) {
-//     console.log('Много!');
-// } else {
+// // if (num < 49) {
+// //     console.log('Неверно!');
+// // } else if (num > 100) {
+// //     console.log('Много!');
+// // } else {
 
+// // }
+
+// // (num == 50) ? console.log('Верно!'): console.log('Неверно!');
+
+// switch (true) {
+//     case num < 49:
+//         console.log('Неверно!');
+//         break;
+//     case num > 100:
+//         console.log('Много!');
+//         break;
+//     case num > 80:
+//         console.log('Все еще много!');
+//         break;
+//     case num == 50:
+//         console.log('Верно!');
+//         break;
+//     default:
+//         console.log('Что то пошло не так');
+//         break;
 // }
 
-// (num == 50) ? console.log('Верно!'): console.log('Неверно!');
+let num = 50;
 
-switch (true) {
-    case num < 49:
-        console.log('Неверно!');
-        break;
-    case num > 100:
-        console.log('Много!');
-        break;
-    case num > 80:
-        console.log('Все еще много!');
-        break;
-    case 50:
-        console.log('Верно!');
-        break;
-    default:
-        console.log('Что то пошло не так');
-        break;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
 }
